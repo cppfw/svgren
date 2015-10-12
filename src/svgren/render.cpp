a834d19b08af372580913802f37ed3b2ff2ca916
Ivan Gagis <igagis@gmail.com>
2015-10-12 23:08:40 +0300

    stuff

---
+++ src/svgren/render.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   render.cpp
+ * Author: ivan
+ * 
+ * Created on October 12, 2015, 11:04 PM
+ */
+
+#include "render.hpp"
+
+
+using namespace svgren;
+
+
+
+
+
---
+++ src/svgren/render.hpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   render.hpp
+ * Author: ivan
+ *
+ * Created on October 12, 2015, 11:04 PM
+ */
+
+#pragma once
+
+#include <svgdom/dom.hpp>
+
+
+namespace svgren{
+
+
+
+}//~namespace
