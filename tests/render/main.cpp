b2818bfe75c18af342649d0e313d36486ba10a15
Ivan Gagis <igagis@gmail.com>
2015-11-06 20:05:23 +0200

    travis

--- tests/render/main.cpp
+++ tests/render/main.cpp
@@ -24,7 +24,7 @@ void processEvent(Display *display, Window window, XImage *ximage, int width, in
 }
 
 int main(int argc, char **argv){
-	auto dom = svgdom::load(papki::FSFile("tiger.svg"));
+	auto dom = svgdom::load(papki::FSFile("camera.svg"));
 	
 	ASSERT_ALWAYS(dom)
 	
