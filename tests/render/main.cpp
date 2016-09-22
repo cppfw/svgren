c4da8d4ff71df5a4680984297f57d946567228b5
Ivan Gagis <igagis@gmail.com>
2016-09-22 23:14:37 +0300

    travis use ruby 2.2.2

--- tests/render/main.cpp
+++ tests/render/main.cpp
@@ -30,49 +30,49 @@ void processEvent(Display *display, Window window, XImage *ximage, int width, in
 #endif
 
 int main(int argc, char **argv){
-	auto dom = svgdom::load(papki::FSFile("tiger.svg"));
+	auto dom = svgdom::load(papki::FSFile("tst.svg"));
 	
 	ASSERT_ALWAYS(dom)
 	
 	
-#if M_OS == M_OS_LINUX
-	XImage *ximage;
-	
-	int width = 800, height=800;
-
-	Display *display = XOpenDisplay(NULL);
-	
-	Visual *visual = DefaultVisual(display, 0);
-	
-	Window window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0, width, height, 1, 0, 0);
-	
-	if(visual->c_class != TrueColor){
-		TRACE_ALWAYS(<< "Cannot handle non true color visual ...\n" << std::endl)
-		return 1;
-	}
-#endif
+//#if M_OS == M_OS_LINUX
+//	XImage *ximage;
+//	
+//	int width = 800, height=800;
+//
+//	Display *display = XOpenDisplay(NULL);
+//	
+//	Visual *visual = DefaultVisual(display, 0);
+//	
+//	Window window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0, width, height, 1, 0, 0);
+//	
+//	if(visual->c_class != TrueColor){
+//		TRACE_ALWAYS(<< "Cannot handle non true color visual ...\n" << std::endl)
+//		return 1;
+//	}
+//#endif
 	
 	unsigned imWidth = 0;
 	unsigned imHeight = 0;
 	auto img = svgren::render(*dom, imWidth, imHeight);
 	
-        TRACE(<< "imWidth = " << imWidth << " imHeight = " << imHeight << " img.size() = " << img.size() << std::endl)
+	TRACE(<< "imWidth = " << imWidth << " imHeight = " << imHeight << " img.size() = " << img.size() << std::endl)
         
 	for(auto& c : img){
 		c = (c & 0xff00ff00) | ((c << 16) & 0xff0000) | ((c >> 16) & 0xff);
 	}
 	
-#if M_OS == M_OS_LINUX
-	ximage = XCreateImage(display, visual, 24, ZPixmap, 0, reinterpret_cast<char*>(&*img.begin()), imWidth, imHeight, 8, 0);
-	
-	XSelectInput(display, window, ButtonPressMask|ExposureMask);
-	
-	XMapWindow(display, window);
-	
-	while(true){
-		processEvent(display, window, ximage, imWidth, imHeight);
-	}
-#endif
+//#if M_OS == M_OS_LINUX
+//	ximage = XCreateImage(display, visual, 24, ZPixmap, 0, reinterpret_cast<char*>(&*img.begin()), imWidth, imHeight, 8, 0);
+//	
+//	XSelectInput(display, window, ButtonPressMask|ExposureMask);
+//	
+//	XMapWindow(display, window);
+//	
+//	while(true){
+//		processEvent(display, window, ximage, imWidth, imHeight);
+//	}
+//#endif
 
 	TRACE_ALWAYS(<< "[PASSED]" << std::endl)
 }
