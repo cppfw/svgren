10450766c4728c4aeda65e1c3e8087029bb88516
Ivan Gagis <igagis@gmail.com>
2017-01-13 16:34:14 +0200

    release 0.4.3

--- src/svgren/render.cpp
+++ src/svgren/render.cpp
@@ -1009,10 +1009,14 @@ std::vector<std::uint32_t> svgren::render(const svgdom::SvgElement& svg, unsigne
 	//unpremultiply alpha
 	for(auto &c : ret){
 		std::uint32_t a = (c >> 24);
-		std::uint32_t r = (c & 0xff) * 0xff / a;
-		std::uint32_t g = ((c >> 8) & 0xff) * 0xff / a;
-		std::uint32_t b = ((c >> 16) & 0xff) * 0xff / a;
-		c = ((a << 24) | (b << 16) | (g << 8) | r);
+		if(a != 0){
+			std::uint32_t r = (c & 0xff) * 0xff / a;
+			std::uint32_t g = ((c >> 8) & 0xff) * 0xff / a;
+			std::uint32_t b = ((c >> 16) & 0xff) * 0xff / a;
+			c = ((a << 24) | (b << 16) | (g << 8) | r);
+		}else{
+			c = 0;
+		}
 	}
 	
 	return ret;
