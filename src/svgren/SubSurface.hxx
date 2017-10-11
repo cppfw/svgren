c0d9b9ab4c093a7eca2ccc64ed42815aa40c6a2b
Ivan Gagis <igagis@gmail.com>
2017-10-11 16:17:25 +0300

    stuff

--- src/svgren/SubSurface.hxx
+++ src/svgren/SubSurface.hxx
@@ -5,9 +5,11 @@ namespace svgren{
 struct SubSurface{
 	std::uint8_t* data; //RGBA premultiplied alpha
 	unsigned stride;
+	
 	unsigned width;
 	unsigned height;
 	
+	//position of subsurface on the canvas
 	unsigned posx;
 	unsigned posy;
 };
