d25660f91f5bde906dbfdff7627fae9b2aa5797d
Ivan Gagis <igagis@gmail.com>
2019-10-16 16:26:23 +0300

    msvs v142 tools

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -13,12 +13,13 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
     #filter and save content to the original file
     #$content | Where-Object {$_ -match 'step[49]'} | Set-Content $_.FullName
 
-    #filter and save content to a new file 
+    #filter and save content to a new file
     ($content -replace '\$\(version\)',"$ver") | Set-Content ($_.BaseName)
 }
 
 
-"%VS141COMNTOOLS%\VsMSBuildCmd.bat"
+"%VS142COMNTOOLS%\VsMSBuildCmd.bat"
+
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debug /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x86 /v:minimal /nologo
@@ -27,6 +28,7 @@ msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debu
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x64 /v:minimal /nologo
 If(!$?){exit 1}
+
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo
@@ -35,6 +37,24 @@ msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debu
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo
 If(!$?){exit 1}
+
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MT /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MT /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+
 Write-NuGetPackage nuget.autopkg
 If(!$?){exit 1}
 Pop-Location
