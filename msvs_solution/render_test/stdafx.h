141f0a4c0f8b0536b63e670fe15717b944003349
Ivan Gagis <igagis@gmail.com>
2020-09-18 17:38:46 +0300

    msvs build fix

--- msvs_solution/render_test/stdafx.h
+++ msvs_solution/render_test/stdafx.h
@@ -9,7 +9,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
-#include <windows.h>
+#include <utki/windows.hpp>
 
 // C RunTime Header Files
 #include <stdlib.h>
