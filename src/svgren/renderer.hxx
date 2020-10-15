5670a2e45cb724f2b4d3653a69fc63ad706767dd
Ivan Gagis <igagis@gmail.com>
2020-10-15 20:32:14 +0300

    msvs nuget update

--- src/svgren/renderer.hxx
+++ src/svgren/renderer.hxx
@@ -4,12 +4,6 @@
 
 #include <utki/config.hpp>
 
-#if M_OS == M_OS_WINDOWS || M_OS_NAME == M_OS_NAME_IOS
-#	include <cairo.h>
-#else
-#	include <cairo/cairo.h>
-#endif
-
 #include <svgdom/visitor.hpp>
 #include <svgdom/finder.hpp>
 #include <svgdom/style_stack.hpp>
