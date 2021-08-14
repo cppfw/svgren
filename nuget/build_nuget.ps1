0cfeddf1aa7a182bf9702b50aa173fa026533891
Ivan Gagis <igagis@gmail.com>
2021-08-14 20:55:04 +0300

    msvs: v141 build

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -28,14 +28,14 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 # msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x64 /v:minimal /nologo
 # If(!$?){exit 1}
 
-# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x64 /v:minimal /nologo
-# If(!$?){exit 1}
-# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo
-# If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
+msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
 
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
