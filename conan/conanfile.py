6adc6982afd684e2433e3f005c32c56dc5bb3e00
Ivan Gagis <igagis@gmail.com>
2025-05-13 14:59:22 +0300

    emsc: fix static lib only

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -24,10 +24,11 @@ class SvgrenConan(ConanFile):
 		self.requires("veg/[>=0.0.0]@cppfw/main", transitive_headers=False)
 
 	def build_requirements(self):
-		if self.settings.os != "Emscripten":
-			self.requires("tst/[>=0.3.29]@cppfw/main", visible=False)
 		self.tool_requires("prorab/[>=2.0.27]@cppfw/main")
 		self.tool_requires("prorab-extra/[>=0.2.57]@cppfw/main")
+
+		if self.settings.os != "Emscripten":
+			self.requires("tst/[>=0.3.29]@cppfw/main", visible=False)
 
 	def config_options(self):
 		if self.settings.os == "Windows":
@@ -54,14 +55,14 @@ class SvgrenConan(ConanFile):
 
 	def build(self):
 		if self.settings.os == "Emscripten":
-			self.run("make $MAKE_INCLUDE_DIRS_ARG config=wasm --directory=src")
+			self.run("make $MAKE_INCLUDE_DIRS_ARG config=emsc --directory=src")
 		else:
 			self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off")
 			self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off test")
 
 	def package(self):
 		if self.settings.os == "Emscripten":
-			src_rel_dir = os.path.join(self.build_folder, "src/out/wasm")
+			src_rel_dir = os.path.join(self.build_folder, "src/out/emsc")
 		else:
 			src_rel_dir = os.path.join(self.build_folder, "src/out/rel")
 
--- config/emsc.mk
+++ config/emsc.mk
@@ -6,7 +6,7 @@ this_cxx := em++
 this_cc := emcc
 this_ar := emar
 
-this_static_lib_only = : true
+this_static_lib_only := true
 
 # TODO: remove the warning suppression when the PR is merged
 # Suppress version-check warning due to https://github.com/conan-io/conan-center-index/pull/26247
