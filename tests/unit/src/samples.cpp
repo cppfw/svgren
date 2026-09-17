146de71332ce6b905e809cd68c60630a73009593
Ivan Gagis <igagis@gmail.com>
2026-09-17 16:47:37 +0300

    fix msvc build

--- tests/unit/src/samples.cpp
+++ tests/unit/src/samples.cpp
@@ -24,9 +24,9 @@ const std::string expected_dir = "expected";
 
 namespace{
 const tst::set set("samples", [](tst::suite& suite){
-    std::vector<std::string> files;
+	std::vector<std::string> files;
 
-    {
+	{
 		const std::regex suffix_regex("^.*\\.svg$");
 		auto all_files = fsif::native_file(data_dir).list_dir();
 
@@ -40,125 +40,128 @@ const tst::set set("samples", [](tst::suite& suite){
 			);
 	}
 
-    suite.add<std::string>(
-        "sample__deprecated_render",
+	suite.add<std::string>(
+		"sample__deprecated_render",
 		{
 #if CFG_CPU_BITS != 64
 			tst::flag::disabled
 #endif
 		},
-        files,
-        [](const auto& p){
-            fsif::native_file in_file(data_dir + p);
+		files,
+		[](const auto& p){
+			fsif::native_file in_file(data_dir + p);
 
-            auto dom = svgdom::load(in_file);
+			auto dom = svgdom::load(in_file);
 
+			// render() is deprecated in favor of reasterize() but there can be users of render in the wild,
+			// so we need to keep it and test it, so here we disable the deprecated warning
 #if CFG_COMPILER == CFG_COMPILER_GCC || CFG_COMPILER == CFG_COMPILER_CLANG
 #	pragma GCC diagnostic push
 #	pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
 #if CFG_COMPILER == CFG_COMPILER_MSVC
-#	pragma warning(disable : 4996)
+#	pragma warning(push)
+#	pragma warning(disable : 4996) // warning about using deprecated function
 #endif
-            // NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations, "false positive")
-            auto res = svgren::render(*dom);
+			// NOLINTNEXTLINE(clang-diagnostic-deprecated-declarations, "false positive")
+			auto res = svgren::render(*dom);
 #if CFG_COMPILER == CFG_COMPILER_MSVC
-#	pragma warning(enable: 4996)
+#	pragma warning(pop)
 #endif
 #if CFG_COMPILER == CFG_COMPILER_GCC
 #	pragma GCC diagnostic pop
 #endif
 
-            auto& img = res.pixels;
+			auto& img = res.pixels;
 
-            fsif::native_file png_file(data_dir + expected_dir + "/" + fsif::not_suffix(in_file.not_dir()) + ".png");
+			fsif::native_file png_file(data_dir + expected_dir + "/" + fsif::not_suffix(in_file.not_dir()) + ".png");
 
 			auto png_var = rasterimage::read_png(png_file);
 
-            tst::check(!png_var.empty(), SL);
+			tst::check(!png_var.empty(), SL);
 
-            tst::check(png_var.get_format() == rasterimage::format::rgba, SL) << "Error: PNG color format is not rgba: " << unsigned(png_var.get_format());
+			tst::check(png_var.get_format() == rasterimage::format::rgba, SL) << "Error: PNG color format is not rgba: " << unsigned(png_var.get_format());
 			tst::check(png_var.get_depth() == rasterimage::depth::uint_8_bit, SL) << "Error: PNG color depth is not 8 bit: " << unsigned(png_var.get_depth());;
-            
-            tst::check(res.dims == png_var.dims(), SL) << "Error: svg dims " << res.dims << " did not match PNG dims " << png_var.dims();
+			
+			tst::check(res.dims == png_var.dims(), SL) << "Error: svg dims " << res.dims << " did not match PNG dims " << png_var.dims();
 
-            tst::check(img.size() == png_var.buffer_size(), SL) << "Error: svg pixel buffer size (" << img.size() << ") did not match PNG pixel buffer size(" << png_var.buffer_size() << ")";
+			tst::check(img.size() == png_var.buffer_size(), SL) << "Error: svg pixel buffer size (" << img.size() << ") did not match PNG pixel buffer size(" << png_var.buffer_size() << ")";
 
 			const auto& png = png_var.get<rasterimage::format::rgba>();
 
-            for(size_t i = 0; i != img.size(); ++i){
-                std::array<uint8_t, 4> rgba = rasterimage::from_32bit_pixel(img[i]);
+			for(size_t i = 0; i != img.size(); ++i){
+				std::array<uint8_t, 4> rgba = rasterimage::from_32bit_pixel(img[i]);
 
-                for(unsigned j = 0; j != rgba.size(); ++j){
-                    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
-                    auto c1 = rgba[j];
-                    auto c2 = png.pixels()[i][j];
-                    if(c1 > c2){
-                        std::swap(c1, c2);
-                    }
+				for(unsigned j = 0; j != rgba.size(); ++j){
+					// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
+					auto c1 = rgba[j];
+					auto c2 = png.pixels()[i][j];
+					if(c1 > c2){
+						std::swap(c1, c2);
+					}
 
-                    if(unsigned(c2 - c1) > tolerance){
+					if(unsigned(c2 - c1) > tolerance){
 						auto png_px = png.pixels()[i];
 
-                        uint32_t pixel = rasterimage::to_32bit_pixel(png_px);
+						uint32_t pixel = rasterimage::to_32bit_pixel(png_px);
 
-                        tst::check(false, SL) << "Error: PNG pixel #" << std::dec << i << " [" << (i % res.dims.x()) << ", " << (i / res.dims.y()) << "]" << " (0x" << std::hex << pixel << ") did not match SVG pixel (0x" << img[i] << ")" << ", png_file = " << png_file.path();
-                    }
-                }
-            }
-        }
-    );
+						tst::check(false, SL) << "Error: PNG pixel #" << std::dec << i << " [" << (i % res.dims.x()) << ", " << (i / res.dims.y()) << "]" << " (0x" << std::hex << pixel << ") did not match SVG pixel (0x" << img[i] << ")" << ", png_file = " << png_file.path();
+					}
+				}
+			}
+		}
+	);
 
 	suite.add<std::string>(
-        "sample",
+		"sample",
 		{
 #if CFG_CPU_BITS != 64
 			tst::flag::disabled
 #endif
 		},
-        files,
-        [](const auto& p){
-            fsif::native_file in_file(data_dir + p);
+		files,
+		[](const auto& p){
+			fsif::native_file in_file(data_dir + p);
 
-            auto dom = svgdom::load(in_file);
+			auto dom = svgdom::load(in_file);
 
-            auto im = svgren::rasterize(*dom);
+			auto im = svgren::rasterize(*dom);
 
-            fsif::native_file png_file(data_dir + expected_dir + "/" + fsif::not_suffix(in_file.not_dir()) + ".png");
+			fsif::native_file png_file(data_dir + expected_dir + "/" + fsif::not_suffix(in_file.not_dir()) + ".png");
 
 			auto png_var = rasterimage::read_png(png_file);
 
-            tst::check(!png_var.empty(), SL);
+			tst::check(!png_var.empty(), SL);
 
-            tst::check(png_var.get_format() == rasterimage::format::rgba, SL) << "Error: PNG color format is not rgba: " << unsigned(png_var.get_format());
+			tst::check(png_var.get_format() == rasterimage::format::rgba, SL) << "Error: PNG color format is not rgba: " << unsigned(png_var.get_format());
 			tst::check(png_var.get_depth() == rasterimage::depth::uint_8_bit, SL) << "Error: PNG color depth is not 8 bit: " << unsigned(png_var.get_depth());;
-            
-            tst::check(im.dims() == png_var.dims(), SL) << "Error: svg dims " << im.dims() << " did not match PNG dims " << png_var.dims();
+			
+			tst::check(im.dims() == png_var.dims(), SL) << "Error: svg dims " << im.dims() << " did not match PNG dims " << png_var.dims();
 
-            tst::check(im.pixels().size() == png_var.buffer_size(), SL) << "Error: svg pixel buffer size (" << im.pixels().size() << ") did not match PNG pixel buffer size(" << png_var.buffer_size() << ")";
+			tst::check(im.pixels().size() == png_var.buffer_size(), SL) << "Error: svg pixel buffer size (" << im.pixels().size() << ") did not match PNG pixel buffer size(" << png_var.buffer_size() << ")";
 
 			const auto& png = png_var.get<rasterimage::format::rgba>();
 
-            for(size_t i = 0; i != im.pixels().size(); ++i){
-                auto rgba = im.pixels()[i];
+			for(size_t i = 0; i != im.pixels().size(); ++i){
+				auto rgba = im.pixels()[i];
 				auto png_px = png.pixels()[i];
 
-                for(unsigned j = 0; j != rgba.size(); ++j){
-                    auto c1 = rgba[j];
-                    auto c2 = png_px[j];
-                    if(c1 > c2){
-                        std::swap(c1, c2);
-                    }
+				for(unsigned j = 0; j != rgba.size(); ++j){
+					auto c1 = rgba[j];
+					auto c2 = png_px[j];
+					if(c1 > c2){
+						std::swap(c1, c2);
+					}
 
-                    if(unsigned(c2 - c1) > tolerance){
-                        uint32_t png_pixel = rasterimage::to_32bit_pixel(png_px);
-                        uint32_t svg_pixel = rasterimage::to_32bit_pixel(rgba);
+					if(unsigned(c2 - c1) > tolerance){
+						uint32_t png_pixel = rasterimage::to_32bit_pixel(png_px);
+						uint32_t svg_pixel = rasterimage::to_32bit_pixel(rgba);
 
-                        tst::check(false, SL) << "Error: PNG pixel #" << std::dec << i << " [" << (i % im.dims().x()) << ", " << (i / im.dims().y()) << "]" << " (0x" << std::hex << png_pixel << ") did not match SVG pixel (0x" << svg_pixel << ")" << ", png_file = " << png_file.path();
-                    }
-                }
-            }
-        }
-    );
+						tst::check(false, SL) << "Error: PNG pixel #" << std::dec << i << " [" << (i % im.dims().x()) << ", " << (i / im.dims().y()) << "]" << " (0x" << std::hex << png_pixel << ") did not match SVG pixel (0x" << svg_pixel << ")" << ", png_file = " << png_file.path();
+					}
+				}
+			}
+		}
+	);
 });
 }
